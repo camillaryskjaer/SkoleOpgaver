5be2445936eab7653300ecf96bfb286335a5d001
Simon hovmand <98515463+SimonHovmand@users.noreply.github.com>
2022-10-25 08:42:27 +0200

    Upload 1.0

---
+++ C#/Øvelser/Øvelse1/Øvelse1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Øvelse1
+
+{
+    class Class1{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast tal antal gange:");
+
+            String str1 = Console.ReadLine();
+            int a = Int32.Parse(str1);
+
+            Console.WriteLine("Indtast tekst:");
+
+            String str2 = Console.ReadLine();
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine(str2);
+                i++;
+            }
+            while (i < a);
+
+        }
+    }
+}
