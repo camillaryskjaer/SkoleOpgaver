396c8355a0ad4bad8aa1985e8be2957f464c6202
Simon hovmand <simonhovmand@gmail.com>
2023-01-30 11:44:53 +0100

    BMI + Terning

---
+++ c#/Kontrolstruktur/BMI/BMI/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Hvad er din højde i m?");
+double height = double.Parse(Console.ReadLine());
+Console.WriteLine(height);
+
+Console.WriteLine("Hvad er din vægt i kg?");
+double Weight = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Weight);
+
+double bmi = (Weight / (height * height));
+
+Console.WriteLine("Din bmi er: " + bmi);
+
+
+
+if (bmi < 18.5)
+{
+    Console.WriteLine("Du er undervægtig");
+}
+
+else if (bmi > 18.5 && bmi <24)
+{
+    Console.WriteLine("Du er normal");
+}
+
+else if(bmi > 24 && bmi < 30) 
+{
+    Console.WriteLine("Du er overvægtig");
+}
+
+else if(bmi > 30 && bmi < 35) 
+{
+    Console.WriteLine("Du er 1 grads overvægtig");
+}
+
+else if(bmi > 35 && bmi < 40)
+{
+    Console.WriteLine("Du er 2 grads overvægtig");
+}
+
+else if (bmi > 40) 
+{
+    Console.WriteLine("Du er 3 grads overvægtig");
+}
---
+++ c#/Kontrolstruktur/Terningen/Terningen/Program.cs
@@ -0,0 +1,44 @@
+﻿Random random = new Random();
+int dice = random.Next(1, 7);
+
+if (dice == 1)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Du slog en etter");
+}
+
+else if (dice == 2)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Du slog en to'er");
+}
+
+else if (dice == 3)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Du slog en tre'er");
+}
+
+else if (dice == 4)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Du slog en fire'er");
+}
+
+else if (dice == 5)
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Du slog en fem'er");
+}
+
+else if (dice == 6)
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Du slog en seks'er");
+}
+
+else if (dice == 7)
+{ 
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.WriteLine("Du slog en syv'er");
+}
