cc76e9d34f8c93821354ede35100a170200e11e7
Simon hovmand <simonhovmand@gmail.com>
2023-01-30 10:11:12 +0100

    Fødselsdag

---
+++ c#/Fødselsdag/Fødselsdag/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Hvornår har du fødselsdag? (ddMMyyyy)");
+DateTime birthday = DateTime.ParseExact(Console.ReadLine(), "ddMMyyyy", null);
+DateTime now = DateTime.Now;
+
+TimeSpan difference = now - birthday;
+
+int years = (int)Math.Floor(difference.TotalDays / 365.2425);
+
+int days = (int)Math.Floor(difference.TotalDays);
+
+Console.WriteLine("Du er " + years + " år og " + days + " dage gammel");
