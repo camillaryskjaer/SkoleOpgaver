90429bcdd42cb2897875fb1cc6c3891032529841
Simon hovmand <simonhovmand@gmail.com>
2023-01-30 09:21:05 +0100

    Upload 2.0

---
+++ c#/Variable og datatyper/Variable og datatyper/Program.cs
@@ -0,0 +1,12 @@
+﻿int length = 16;
+double width = 5.1;
+
+// Console.WriteLine(length * width); = 81,6
+
+int area = length * width;
+
+// Det er ikke muligt at gange en ind og double, med datatypen "int area". Da svaret giver 81,6 og derfor skal være en double.
+
+double d = length * width;
+
+// Fordi at vi bruger datatypen "double"
