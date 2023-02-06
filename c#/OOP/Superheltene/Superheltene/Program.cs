b571edfe6ac49e4d729207aafec411430c390bed
Simon hovmand <simonhovmand@gmail.com>
2023-02-06 09:27:11 +0100

    Superhero

---
+++ c#/OOP/Superheltene/Superheltene/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Superhero
+{
+    private string _name;
+    private string _mask;
+    private string _outfit;
+    private List<Abilities> _abilities;
+    private List<Enamy> _enamy;
+
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+}
+
+internal class Abilities
+{
+    private string _invisibility;
+    private string _strenght;
+    private string _teleport;
+}
+
+internal class Enamy
+{
+    private string _name;
+    private string _mask;
+    private string _outfit;
+    private List<Superhero> _enamy;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+}
