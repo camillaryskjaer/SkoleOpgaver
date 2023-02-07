89af1e7b46d2399622ec976a4c16c7c9ebedb780
Simon hovmand <simonhovmand@gmail.com>
2023-02-07 13:23:35 +0100

    Black Jack

---
+++ c#/OOP/Black Jack/Black Jack/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Black_Jack
+{
+    public struct Card
+    {
+        string suit = "";
+        byte value = 0;
+        byte point = 0;
+
+        public Card(string s, byte v, byte p)
+        {
+            Suit = s;
+            Value = v;
+            Point = p;
+        }
+
+        public string Suit { get => suit; private set => suit = value; }
+        public byte Value { get => value; private set => this.value = value; }
+        public byte Point { get => point; private set => point = value; }
+
+
+
+        public override string ToString()
+        {
+            return this.suit + " : " + this.Value;
+        }
+    }
+}
--- c#/OOP/Black Jack/Black Jack/Program.cs
+++ c#/OOP/Black Jack/Black Jack/Program.cs
@@ -1,1 +1,119 @@
-﻿
+﻿namespace Black_Jack
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Card[] deck = new Card[52];
+
+            string[] suit = { "Hearts", "Dimonds", "Spades", "Clubs" };
+
+            byte deckCounter = 0;
+
+            //Creating deck
+
+            for (int i = 0; i < suit.Length; i++)
+            {
+                for (byte j = 1; j <= 13; j++)
+                {
+                    deck[deckCounter] = new Card(suit[i], j, j);
+                    deckCounter++;
+                }
+            }
+
+            //Shuffle
+            deck = Shuffle(deck);
+
+            //Write deck shuffled
+            //foreach (Card item in deck)
+            //{
+            //    Console.WriteLine(item.Suit + ":" + item.Value);
+            //}
+            //Console.ReadKey();
+
+            while (true)
+            {
+                byte count = 0;
+
+                byte playercount = 0;
+
+                byte computercount = 0;
+
+                byte choise = 2;
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("Dine kort er: " + deck[count].ToString());
+                    playercount += deck[count].Value;
+                    count++;
+                }
+
+                Console.WriteLine(playercount);
+
+                Console.WriteLine(" -----");
+
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("Computerens kort er: " + deck[count].ToString());
+                    computercount += deck[count].Value;
+                    count++;
+                }
+
+                Console.WriteLine(computercount);
+
+                if (playercount <= 21 || computercount <= 21)
+                {
+
+                    Console.WriteLine("Ønsker du at trække et kort mere? 1 = YES - 2 = STAND");
+                    choise = Convert.ToByte(Console.ReadLine());
+
+                    if (choise == 1)
+                    {
+                        Console.WriteLine("Dit kort er: " + deck[count].ToString());
+                        playercount += deck[count].Value;
+                        count++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Computerens kort er: " + deck[count].ToString());
+                        computercount += deck[count].Value;
+                        count++;
+                    }
+
+                }
+                else if (playercount >= 21)
+                {
+                    Console.WriteLine("Du har tabt");
+                    Console.WriteLine("Din count: " + playercount);
+                    Console.WriteLine("Computer count: " + computercount);
+                }
+                else
+                {
+                    Console.WriteLine("Du har vunder over computeren");
+                    Console.WriteLine("Din count: " + playercount);
+                    Console.WriteLine("Computer count: " + computercount);
+                }
+            }
+        }
+
+        //Shuffle cards
+        public static Card[] Shuffle(Card[] sorted)
+        {
+            Random rand = new Random();
+
+            for (int i = 0;i < 200;i++)
+            {
+                int index1 = rand.Next(0, sorted.Length);
+                int index2 = rand.Next(0, sorted.Length);
+
+                Card placeholder = sorted[index1];
+
+                sorted[index1] = sorted[index2];
+
+                sorted[index2] = placeholder;
+            }
+            return sorted;
+        }
+    }
+}
