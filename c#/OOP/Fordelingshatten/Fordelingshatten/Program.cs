ecc680bcb02d4f96d75965f627b8701098ef3b7e
Simon hovmand <simonhovmand@gmail.com>
2023-02-08 09:23:41 +0100

    Fordelingshatten

---
+++ c#/OOP/Fordelingshatten/Fordelingshatten/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Fordelingshatten
+{
+    public class Program
+    {
+        enum Colleges { Gryffindor, Hufflepuff, Ravenclaw, Slytherin }
+
+        public static void Main(string[] args)
+        {
+            Sortinghat sorter = new Sortinghat();
+
+            Console.WriteLine(sorter.Sorter());
+        }
+    }
+}
---
+++ c#/OOP/Fordelingshatten/Fordelingshatten/Sortinghat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fordelingshatten
+{
+    public class Sortinghat
+    {
+        public enum Colleges { Gryffindor, Hufflepuff, Ravenclaw, Slytherin }
+
+        public Colleges Sorter()
+        {
+            Random random = new Random();
+            Type type = typeof(Colleges);
+
+            Array values = type.GetEnumValues();
+
+            int index = random.Next(values.Length);
+            Colleges value = (Colleges)values.GetValue(index);
+
+            return value;
+        }
+    }
+}
